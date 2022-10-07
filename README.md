240024e029892b4aab1041f553dd8360ffa3df73
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-10-06 21:50:25 -0400

    adding setup_handlers

--- README.md
+++ README.md
@@ -97,7 +97,42 @@ local DEFAULT_SETTINGS = {
 
 # Setup handlers usage
 
-To Be Implemented
+# Setup handlers usage
+
+The `setup_handlers()` function provides a dynamic way of setting up sources and any other needed logic, It can also do that during runtime.
+
+```lua
+local dap = require("dap")
+
+require ('mason-nvim-dap').setup({
+    ensure_installed = {'stylua', 'jq'}
+})
+
+require 'mason-nvim-dap'.setup_handlers {
+    function(source_name)
+      -- all sources with no handler get passed here
+    end,
+    python = function()
+        dap.adapters.python = {
+	        type = "executable",
+	        command = "/usr/bin/python3",
+	        args = {
+		        "-m",
+		        "debugpy.adapter",
+	        },
+        }
+
+        dap.configurations.python = {
+	        {
+		        type = "python",
+		        request = "launch",
+		        name = "Launch file",
+		        program = "${file}", -- This configuration will launch the current file if used.
+	        },
+        }
+    end,
+}
+```
 
 # Available Dap Adapters
 
--- lua/mason-nvim-dap/init.lua
+++ lua/mason-nvim-dap/init.lua
@@ -41,6 +41,49 @@ function M.get_installed_sources()
 	end, registry.get_installed_package_names())
 end
 
+function M.setup_handlers(handlers)
+	local Optional = require('mason-core.optional')
+	local source_mappings = require('mason-nvim-dap.mappings.source')
+	local registry = require('mason-registry')
+	local notify = require('mason-core.notify')
+
+	local default_handler = Optional.of_nilable(handlers[1])
+
+	_.each(function(handler)
+		if type(handler) == 'string' and not source_mappings.nvim_dap_to_package[handler] then
+			notify(
+				('mason-nvim-dap.setup_handlers: Received handler for unknown dap source name: %s.'):format(handler),
+				vim.log.levels.WARN
+			)
+		end
+	end, _.keys(handlers))
+
+	---@param pkg_name string
+	local function get_source_name(pkg_name)
+		return Optional.of_nilable(source_mappings.package_to_nvim_dap[pkg_name])
+	end
+
+	local function call_handler(source_name)
+		log.fmt_trace('Checking handler for %s', source_name)
+		Optional.of_nilable(handlers[source_name]):or_(_.always(default_handler)):if_present(function(handler)
+			log.fmt_trace('Calling handler for %s', source_name)
+			local ok, err = pcall(handler, source_name)
+			if not ok then
+				vim.notify(err, vim.log.levels.ERROR)
+			end
+		end)
+	end
+
+	local installed_sources = _.filter_map(get_source_name, registry.get_installed_package_names())
+	_.each(call_handler, installed_sources)
+	registry:on(
+		'package:install:success',
+		vim.schedule_wrap(function(pkg)
+			get_source_name(pkg.name):if_present(call_handler)
+		end)
+	)
+end
+
 ---Get a list of available sources in mason-registry
 ---@param filter { filetype: string | string[] }?: (optional) Used to filter the list of source names.
 --- The available keys are
