228b4036a136f07da0c22df647f3915c8427acf4
Micah Halter <micah@balena.io>
2022-10-21 12:44:38 -0400

    add descriptive names to DAP configurations

--- lua/mason-nvim-dap/mappings/configurations.lua
+++ lua/mason-nvim-dap/mappings/configurations.lua
@@ -6,13 +6,13 @@ local M = {}
 M.delve = {
 	{
 		type = 'delve',
-		name = 'Debug',
+		name = 'Delve: Debug',
 		request = 'launch',
 		program = '${file}',
 	},
 	{
 		type = 'delve',
-		name = 'Debug test', -- configuration for debugging test files
+		name = 'Delve: Debug test', -- configuration for debugging test files
 		request = 'launch',
 		mode = 'test',
 		program = '${file}',
@@ -20,7 +20,7 @@ M.delve = {
 	-- works with go.mod packages and sub packages
 	{
 		type = 'delve',
-		name = 'Debug test (go.mod)',
+		name = 'Delve: Debug test (go.mod)',
 		request = 'launch',
 		mode = 'test',
 		program = './${relativeFileDirname}',
@@ -33,7 +33,7 @@ M.bash = {
 	{
 		type = 'bash',
 		request = 'launch',
-		name = 'Launch file',
+		name = 'Bash: Launch file',
 		program = '${file}',
 		cwd = '${fileDirname}',
 		pathBashdb = BASHDB_DIR .. '/bashdb',
@@ -52,14 +52,14 @@ M.python = {
 		-- The first three options are required by nvim-dap
 		type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
 		request = 'launch',
-		name = 'Launch file',
+		name = 'Python: Launch file',
 		program = '${file}', -- This configuration will launch the current file if used.
 	},
 }
 
 M.codelldb = {
 	{
-		name = 'Launch',
+		name = 'LLDB: Launch',
 		type = 'lldb',
 		request = 'launch',
 		program = function()
@@ -73,7 +73,7 @@ M.codelldb = {
 
 M.node2 = {
 	{
-		name = 'Launch',
+		name = 'Node2: Launch',
 		type = 'node2',
 		request = 'launch',
 		program = '${file}',
@@ -84,7 +84,7 @@ M.node2 = {
 	},
 	{
 		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
-		name = 'Attach to process',
+		name = 'Node2: Attach to process',
 		type = 'node2',
 		request = 'attach',
 		processId = require('dap.utils').pick_process,
@@ -93,7 +93,7 @@ M.node2 = {
 
 M.chrome = {
 	{
-		name = 'Debug with Chrome',
+		name = 'Chrome: Debug',
 		type = 'chrome',
 		request = 'attach',
 
@@ -108,7 +108,7 @@ M.chrome = {
 
 M.firefox = {
 	{
-		name = 'Debug with Firefox',
+		name = 'Firefox: Debug',
 		type = 'firefox',
 		request = 'launch',
 
@@ -123,7 +123,7 @@ M.php = {
 	{
 		type = 'php',
 		request = 'launch',
-		name = 'Listen for Xdebug',
+		name = 'PHP: Listen for Xdebug',
 		port = 9000,
 	},
 }
@@ -131,7 +131,7 @@ M.php = {
 M.coreclr = {
 	{
 		type = 'coreclr',
-		name = 'launch - netcoredbg',
+		name = 'NetCoreDbg: Launch',
 		request = 'launch',
 		program = function()
 			return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
