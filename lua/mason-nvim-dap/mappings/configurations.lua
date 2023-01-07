749e09e8f7db3ade32221813ed1e60f63395b69f
Nikolaj Sidorenco <nsi@in-commodities.com>
2023-01-07 15:10:24 +0100

    formatting

--- lua/mason-nvim-dap/mappings/configurations.lua
+++ lua/mason-nvim-dap/mappings/configurations.lua
@@ -130,25 +130,23 @@ M.php = {
 	},
 }
 
-local function get_dll ()
-  return coroutine.create(function(dap_run_co)
-    local items = vim.fn.globpath(vim.fn.getcwd(), "**/bin/Debug/**/*.dll", 0, 1)
-    local opts = {
-      format_item = function (path)
-          return vim.fn.fnamemodify(path, ":t")
-        end
-    }
-    local function cont (choice)
-      if choice == nil then
-        return nil
-      else
-        coroutine.resume(dap_run_co, choice)
-      end
-    end
-
-    vim.ui.select(items, opts, cont)
-  end
-)
+local function get_dll()
+	return coroutine.create(function(dap_run_co)
+		local items = vim.fn.globpath(vim.fn.getcwd(), '**/bin/Debug/**/*.dll', 0, 1)
+		local opts = {
+			format_item = function(path)
+				return vim.fn.fnamemodify(path, ':t')
+			end,
+		}
+		local function cont(choice)
+			if choice == nil then
+				return nil
+			else
+				coroutine.resume(dap_run_co, choice)
+			end
+		end
+		vim.ui.select(items, opts, cont)
+	end)
 end
 
 M.coreclr = {
