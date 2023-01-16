daa8b6de3b1284d9f02031fe0c458ba172e5e514
Nikolaj Sidorenco <108528329+nsi-inco@users.noreply.github.com>
2023-01-16 17:31:09 +0100

    Improve netcoredbg configuration (#31)
    * Select dlls with ui.select

--- lua/mason-nvim-dap/mappings/configurations.lua
+++ lua/mason-nvim-dap/mappings/configurations.lua
@@ -130,14 +130,32 @@ M.php = {
 	},
 }
 
+local function get_dll()
+	return coroutine.create(function(dap_run_co)
+		local items = vim.fn.globpath(vim.fn.getcwd(), '**/bin/Debug/**/*.dll', 0, 1)
+		local opts = {
+			format_item = function(path)
+				return vim.fn.fnamemodify(path, ':t')
+			end,
+		}
+		local function cont(choice)
+			if choice == nil then
+				return nil
+			else
+				coroutine.resume(dap_run_co, choice)
+			end
+		end
+		vim.ui.select(items, opts, cont)
+	end)
+end
+
 M.coreclr = {
 	{
 		type = 'coreclr',
 		name = 'NetCoreDbg: Launch',
 		request = 'launch',
-		program = function()
-			return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-		end,
+		cwd = '${fileDirname}',
+		program = get_dll,
 	},
 }
 
