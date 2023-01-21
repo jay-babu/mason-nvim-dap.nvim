47bfccd385bfda81a40468ad5778b43f8cd304bc
chomosuke <a13323600@gmail.com>
2023-01-21 21:44:53 +1300

    Fix dart & flutter path on windows

--- lua/mason-nvim-dap/mappings/configurations.lua
+++ lua/mason-nvim-dap/mappings/configurations.lua
@@ -214,13 +214,20 @@ M.kotlin = {
 	},
 }
 
+local function flutter_path()
+	return vim.loop.os_uname().sysname == 'Windows_NT' and 'C:\\src\\flutter' or os.getenv('HOME') .. '/flutter'
+end
+
 M.dart = {
 	{
 		type = 'dart',
 		request = 'launch',
 		name = 'Launch flutter',
-		dartSdkPath = os.getenv('HOME') .. '/flutter/bin/cache/dart-sdk/',
-		flutterSdkPath = os.getenv('HOME') .. '/flutter',
+		-- windows don't really have a standard install path
+		-- best effort guess is the instructed install path from:
+		-- https://docs.flutter.dev/get-started/install/windows
+		dartSdkPath = flutter_path() .. '/bin/cache/dart-sdk/',
+		flutterSdkPath = flutter_path(),
 		program = '${workspaceFolder}/lib/main.dart',
 		cwd = '${workspaceFolder}',
 	},
