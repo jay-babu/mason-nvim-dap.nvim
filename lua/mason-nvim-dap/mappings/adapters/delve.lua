85dbc0a8c220076062f722e687d8ec037f0566cd
Anton Petrov <petrova@vmware.com>
2023-04-20 16:57:00 +0300

    Revert "perf: Lazily require adapter mappings (#70)"
    This reverts commit 9816e2aa07872636018dc501753c119943c592f7.

--- lua/mason-nvim-dap/mappings/adapters/delve.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#go-using-delve-directly
-return {
-	type = 'server',
-	port = '${port}',
-	executable = {
-		command = vim.fn.exepath('dlv'),
-		args = { 'dap', '-l', '127.0.0.1:${port}' },
-	},
-}
