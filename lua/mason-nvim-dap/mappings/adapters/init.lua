e0a78fc5fa4ce7e06239a8cfc59ef5e1ec2bcb6c
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2023-04-21 14:24:15 +0200

    Fixes the problem of mason requiring adapters not mapped by this plugin

--- lua/mason-nvim-dap/mappings/adapters/init.lua
+++ lua/mason-nvim-dap/mappings/adapters/init.lua
@@ -2,8 +2,42 @@
 
 local M = {}
 
+---Convenience function for creating a set from a list.
+---@param list table A list which's values are to be used as indexes in the set.
+---@return table A table with the values from the list set as indexes.
+local function Set(list)
+	local set = {}
+	for _, val in ipairs(list) do
+		set[val] = true
+	end
+	return set
+end
+
+---Set of adapters that can be mapped directly from this module.
+---I.e. They should be able to be found with for example:
+--->lua
+---	require('mason-nvim-dap.mappings.adapters.chrome')
+---<
+local OWN_MAPS = Set {
+	'bash',
+	'chrome',
+	'codelldb',
+	'coreclr',
+	'cppdbg',
+	'dart',
+	'delve',
+	'firefox',
+	'kotlin',
+	'mix_task',
+	'node2',
+	'php',
+	'python',
+}
+
 local META = {}
 function META.__index(table, key)
+	if not OWN_MAPS[key] then return nil end
+
 	local adapter = require('mason-nvim-dap.mappings.adapters.' .. key)
 	table[key] = adapter
 	return adapter
