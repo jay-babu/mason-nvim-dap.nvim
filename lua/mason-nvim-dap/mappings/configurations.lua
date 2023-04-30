f20c0c1a4a944b7166f1d23827033bf4d28702bd
Samuel Cobb <sjcobb2003@gmail.com>
2023-05-01 00:24:19 +0100

    fix: use ghci-dap and ignore-dot-ghci

--- lua/mason-nvim-dap/mappings/configurations.lua
+++ lua/mason-nvim-dap/mappings/configurations.lua
@@ -251,8 +251,10 @@ M.haskell = {
 		ghciEnv = vim.empty_dict(),
 		ghciPrompt = 'ghci>',
 		-- Adjust the prompt to the prompt you see when you invoke the stack ghci command below
-		ghciInitialPrompt = 'ghci>',
-		ghciCmd = 'stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show',
+		ghciInitialPrompt = '>',
+		ghciCmd = 'stack ghci --with-ghc='
+			.. vim.fn.exepath('ghci-dap')
+			.. ' --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show --ghci-options -ignore-dot-ghci',
 	},
 }
 
