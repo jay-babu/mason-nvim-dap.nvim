2bff39c891f89fb58096ea67ef404db305a9477b
Eddy Castillo <dyskette@gmail.com>
2024-06-29 21:57:42 -0600

    fix(netcoredbg): failing to start on Windows (#133)

--- lua/mason-nvim-dap/mappings/adapters/coreclr.lua
+++ lua/mason-nvim-dap/mappings/adapters/coreclr.lua
@@ -1,5 +1,13 @@
-return {
+local M = {
 	type = 'executable',
 	command = vim.fn.exepath('netcoredbg'),
 	args = { '--interpreter=vscode' },
 }
+
+if vim.fn.has('win32') == 1 then
+	M.options = {
+		detached = false,
+	}
+end
+
+return M
