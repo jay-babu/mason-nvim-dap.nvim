959b71f26eaef67d68db687fb721af693680c86e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-09 21:11:07 -0400

    refactor: uncommented js and commented out the other types

--- lua/mason-nvim-dap/mappings/filetypes.lua
+++ lua/mason-nvim-dap/mappings/filetypes.lua
@@ -10,7 +10,7 @@ local M = {
 	['dart'] = { 'dart' },
 	['delve'] = { 'go' },
 	['firefox'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
-	-- ['js'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
+	['js'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
 	['kotlin'] = { 'kotlin' },
 	['mix_task'] = { 'elixir' },
 	['node2'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
@@ -18,11 +18,11 @@ local M = {
 	['python'] = { 'python' },
 	['haskell'] = { 'haskell' },
 	['erlang'] = { 'erlang' },
-	['pwa-node'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
-	['pwa-chrome'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
-	['pwa-msedge'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
-	['pwa-terminal'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
-	['pwa-extensionHost'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
+	-- ['pwa-node'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
+	-- ['pwa-chrome'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
+	-- ['pwa-msedge'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
+	-- ['pwa-terminal'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
+	-- ['pwa-extensionHost'] = { 'javascriptreact', 'typescriptreact', 'typescript', 'javascript' },
 }
 
 return M
