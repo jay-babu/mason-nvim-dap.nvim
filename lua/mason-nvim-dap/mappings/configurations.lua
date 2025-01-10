4225d4bfdcae2064e63d463183b7f33315e4f61d
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-01-10 03:41:44 -0400

    feat: added config for jests tests

--- lua/mason-nvim-dap/mappings/configurations.lua
+++ lua/mason-nvim-dap/mappings/configurations.lua
@@ -206,6 +206,21 @@ M.js = {
       protocol = 'inspector',
     },
   },
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Debug Jest Tests",
+    -- trace = true, -- include debugger info
+    runtimeExecutable = "node",
+    runtimeArgs = {
+      "./node_modules/jest/bin/jest.js",
+      "--runInBand",
+    },
+    rootPath = "${workspaceFolder}",
+    cwd = "${workspaceFolder}",
+    console = "integratedTerminal",
+    internalConsoleOptions = "neverOpen",
+  },
 }
 
 M.php = {
